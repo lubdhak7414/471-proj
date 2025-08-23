fa9d90febd337a5f545b67977b46097cccbfba20
InteSir <98264525+InteSir@users.noreply.github.com>
2025-08-23 11:17:53 +0600

    Add files via upload

---
+++ backend/models/message.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  conversationId: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
