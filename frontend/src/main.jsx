8d9788fd24522b7bbf36a5b4a849bb9d2aa30b89
rodoshi-is-a-dev <rodoshi.tasnim.rahman@g.bracu.ac.bd>
2025-08-24 15:42:52 +0600

    Revert "Update main.jsx"
    This reverts commit 5aeffeb840084db1a62cff688ea68e86d60eed26.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,30 +11,8 @@ import ProtectedRoutes from './context/ProtectedRoutes.jsx'; // Make sure the pa
 import './index.css';
 import Layout from './Layout.jsx'; //
 
-
-
-import SearchTechnicians from './components/SearchTechnician.jsx';
-import PaymentGateway from './components/PaymentGateway.jsx';
-import TechnicianDashboard from './components/TechnicianDashboard.jsx';
-
-import { SocketProvider } from './context/socket.provider.jsx'; // Import SocketProvider
-
-
-
 // Define your routes
 const router = createBrowserRouter([
-  {
-    path:'/search',
-    element: <SearchTechnicians />
-  },
-  {
-    path:'/dashboard',
-    element: <TechnicianDashboard />
-  },
-    {
-    path:'/payment',
-    element: <PaymentGateway />
-  },
   {
     path: '/',
     element: <LandingPage />,
@@ -64,11 +42,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <SocketProvider> {/* Add SocketProvider here */}
-        <Layout>
-          <RouterProvider router={router} />
-        </Layout>
-      </SocketProvider>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
     </AuthProvider>
   </React.StrictMode>
 );
